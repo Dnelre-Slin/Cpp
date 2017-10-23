56edbaf236c7071ea51a60379ddfc5023f65d530
Dnelre-Slin <nilsheggem@hotmail.com>
2017-10-24 00:40:58 +0200

    Calculated pi

--- Math/Math/Main.cpp
+++ Math/Math/Main.cpp
@@ -78,10 +78,57 @@ void test2()
 
 }
 
+double getPi1(int index, int end, double *a_doubles)
+{
+	double pi = a_doubles[index++];
+	if (index >= end)
+		return pi;
+	return pi + (1 / getPi1(index, end, a_doubles));
+}
+
+double getPi2(double nr, double end)
+{
+	if (nr >= end)
+		return 6;
+	return 6 + (pow(nr, 2) / getPi2(nr + 2, end));
+}
+
+void test3()
+{
+	const int amount = 1000;
+	int index = 0;
+	int end = amount - 1;
+	double *a_doubles = new double[amount];
+	//double a_doubles[amount];
+
+	a_doubles[0] = 3;
+	a_doubles[1] = 7;
+	a_doubles[2] = 15;
+	a_doubles[3] = 1;
+	a_doubles[4] = 292;
+
+	for (int i = 5; i < amount; i++)
+	{
+		a_doubles[i] = 1;
+	}
+
+	double pi1 = getPi1(index, end, a_doubles);
+
+	delete[] a_doubles;
+
+	//std::cout << "PI = " << pi << '\n';
+	printf("PI = %.40f\n", pi1);
+
+	double pi2 = 3 + (1 / getPi2(3, 5000));
+
+	printf("PI = %.40f\n", pi2);
+}
+
 int main()
 {
 	//test1();
-	test2();
+	//test2();
+	test3();
 	system("pause");
 	return 0;
 }
